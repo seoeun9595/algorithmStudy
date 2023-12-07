79c54024905f17f4428e8df74e067660e62eca66
Seo-eun <151019751+seoeun9595@users.noreply.github.com>
2023-12-07 23:01:24 +0900

    [level unrated] Title: 옹알이 (1), Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/120956. 옹알이 （1）/옹알이 （1）.py
+++ 프로그래머스/unrated/120956. 옹알이 （1）/옹알이 （1）.py
@@ -3,8 +3,7 @@ def solution(babbling):
     cnt = 0
     for i in babbling:
         for j in can:
-            i = i.replace(j, " ")
-        i = i.replace(" ", "")
-        if len(i) == 0:
+            i = i.replace(j, "1")
+        if i.isdigit():
             cnt += 1
     return cnt
