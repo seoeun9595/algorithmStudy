b21a3aec261d1798d9dcb3a4da99de29f1bad551
Seo-eun <151019751+seoeun9595@users.noreply.github.com>
2023-12-08 09:29:51 +0900

    [level unrated] Title: A로 B 만들기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/120886. A로 B 만들기/A로 B 만들기.py
+++ 프로그래머스/unrated/120886. A로 B 만들기/A로 B 만들기.py
@@ -1,4 +1,2 @@
 def solution(before, after):
-    for i in before:
-        after = after.replace(i, "", 1)
-    return (1 if len(after) == 0 else 0)
+    return (1 if sorted(before) == sorted(after) else 0)
